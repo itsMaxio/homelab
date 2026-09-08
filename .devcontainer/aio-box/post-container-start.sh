dcc5424a683e998ae16fe9432ba66bf01e4b35df
Maxio <29357824+itsMaxio@users.noreply.github.com>
2026-09-08 12:41:14 +0200

    chore(devcontainer): enhance devcontainers

---
+++ .devcontainer/aio-box/post-container-start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eu
+
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+if [ -d /root/.sshtemplate ]; then
+    cp -a /root/.sshtemplate/. /root/.ssh/
+fi
+
+find /root/.ssh -type f -exec chmod 600 {} \;
+find /root/.ssh -type d -exec chmod 700 {} \;
